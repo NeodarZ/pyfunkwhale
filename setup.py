274026d0fd21f5e96d7109e52f7d910a0b1d7225
neodarz <neodarz@neodarz.net>
2020-04-11 16:27:34 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name="pyfunkwhale",
+    description="A simple funkwhale API client library",
+    version="0.0.1",
+    author="neodarz",
+    author_email="neodarz@neodarz.net",
+    url="https://git.neodarz.net/neodarz/pyfunkwhale.git",
+    license="GPLv3",
+    packages=["pyfunkwhale"],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later '
+        '(GPLv3+)',
+        'Programming Language :: Python :: 3']
+)
